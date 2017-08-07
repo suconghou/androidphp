5a76cb8dc34f6d701459004414abde65b246d4c7
suconghou <1126045770@qq.com>
2017-08-07 19:26:18 +0800

    update php version and http

--- http/microhttp.go
+++ http/microhttp.go
@@ -6,7 +6,7 @@ import (
 	"path/filepath"
 )
 
-var doc string = "/mnt/sdcard/external_sd"
+var doc = "/mnt/sdcard/external_sd"
 
 func main() {
 	http.HandleFunc("/", routeMatch)
@@ -14,10 +14,8 @@ func main() {
 }
 
 func routeMatch(w http.ResponseWriter, r *http.Request) {
-	var files []string
-	if r.URL.Path == "/" {
-		files = []string{"index.html"}
-	} else {
+	files := []string{"index.html"}
+	if r.URL.Path != "/" {
 		files = []string{r.URL.Path, filepath.Join(r.URL.Path, "index.html")}
 	}
 	if !tryFiles(files, w, r) {
@@ -27,7 +25,7 @@ func routeMatch(w http.ResponseWriter, r *http.Request) {
 
 func tryFiles(files []string, w http.ResponseWriter, r *http.Request) bool {
 	for _, file := range files {
-		var realpath string = filepath.Join(doc, file)
+		realpath := filepath.Join(doc, file)
 		if f, err := os.Stat(realpath); err == nil {
 			if f.Mode().IsRegular() {
 				http.ServeFile(w, r, realpath)
